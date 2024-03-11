73bf48df71139a03eb3ceba12aebd7b494b5b6aa
skacel-t <156572655+skacel-t@users.noreply.github.com>
2024-03-11 18:05:03 +0100

    more changes

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
+
 class Product(Base):
     __tablename__ = 'products'
     
@@ -21,23 +22,7 @@ class Product(Base):
         return f"{self.product_id} | {self.product_name} | {self.product_quantity} | {self.product_price} | {self.date_updated}"
 
 
-def menu():
-	while True:
-		print("""
-			\r--- STORE INVENTORY ---
-			\r1) Enter 'v' to view a product
-			\r2) Enter 'a' to add a new product
-			\r3) Enter 'b' to make a backup of the database
-			\r4) Enter 'q' to exit""")
-
-		choice = input("What would you like to do?  ")
-		choice = choice.lower()
-		if choice in ['v', 'a', 'b', 'q']:
-			return choice
-		else: 
-			input("Input Error! Please choose one of the options above. Press enter to try again.")
-
-
+# Let's begin with the functions that will be used in the app
 def clean_quantity(quantity_str):
 	return_quantity = int(quantity_str)
 	return return_quantity
@@ -56,7 +41,6 @@ def clean_date(date_str):
 	return_date = datetime.date(year, month, day)
 	return return_date
 
-
 def clean_id(id_str, id_options):
 	try:
 		id_int = int(id_str)
@@ -68,8 +52,23 @@ def clean_id(id_str, id_options):
 			return id_int
 		else:
 			input("There is no product associated with this ID, please choose an ID from the ID Options. \nPress enter to try again.")
-			return None
+			return None	
 
+def menu():
+	while True:
+		print("""
+			\r--- STORE INVENTORY ---
+			\r1) Enter 'v' to view a product
+			\r2) Enter 'a' to add a new product
+			\r3) Enter 'b' to make a backup of the database
+			\r4) Enter 'q' to exit""")
+
+		choice = input("What would you like to do?  ")
+		choice = choice.lower()
+		if choice in ['v', 'a', 'b', 'q']:
+			return choice
+		else: 
+			input("Input Error! Please choose one of the options above. Press enter to try again.")
 
 def display_product(id_num):
 	product = session.query(Product).filter(Product.product_id==id_num).first()
@@ -80,9 +79,8 @@ def display_product(id_num):
 		\rPrice: ${product.product_price/100}
 		\rDate Updated: {formatted_date}""")
 
-
 def add_product():
-	name = input("Product name:  ")
+	name = input("\nProduct name:  ")
 	while True:
 		try:
 			quantity = input("Quantity:  ")
@@ -91,7 +89,6 @@ def add_product():
 			print("Please enter a whole number.")
 		else: 
 			break
-
 	while True:
 		try:
 			price = input("Price:  ")
@@ -110,19 +107,13 @@ def add_product():
 	if product_in_db == None:
 		new_product = Product(product_name=name, product_quantity=quantity, product_price=price, date_updated=date)
 		session.add(new_product)
-
-		print("New product added!")
+		input("New product added! Press enter to return to menu.")
 	else:
 		product_in_db.product_quantity = quantity
 		product_in_db.product_price = price
 		product_in_db.date_updated = date
-
-		print("Existing product info updated!")
-
+		input("Existing product info updated! Press enter to return to menu.")
 	session.commit()
-
-
-
 
 def backup():
 	# be careful to always create a new fresh file... maybe "a" is not the way
@@ -135,7 +126,6 @@ def backup():
 			upload_data = {}
 			upload_data['product_name'] = product.product_name
 			upload_data['product_quantity'] = product.product_quantity
-
 			upload_data['product_price'] = f"${product.product_price/100}"
 
 			day = product.date_updated.day
@@ -144,37 +134,6 @@ def backup():
 			upload_data['date_updated'] = f"{month}/{day}/{year}"
 
 			writer.writerow(upload_data)
-
-
-def app():
-	while True:
-		choice = menu()
-		choice = choice.lower()
-		if choice == 'v':
-			id_options = []
-			for product in session.query(Product):
-				id_options.append(product.product_id)
-			id_error = True
-			while id_error:
-				id_choice = input(f"""
-					\rID options: {id_options}
-					\rEnter the product ID of the item you would like to view:  """)
-				id_choice = clean_id(id_choice, id_options)
-				if type(id_choice) == int:
-					id_error = False 
-			display_product(id_choice)
-		elif choice == 'a':
-			# add product
-			add_product()
-		elif choice == 'b':
-			# create csv file backup
-			backup()
-			input("Backup has been created. Press enter to return to main menu.")
-		elif choice == 'q':
-			print("Goodbye!")
-			return None
-
-
 
 def add_csv():
 	with open("inventory.csv", newline="") as csvfile:
@@ -194,17 +153,42 @@ def add_csv():
 				# if we have a more recent date, we overwrite the data...
 				date_in_db = product_in_db.date_updated
 				if date > date_in_db:
-					session.delete(product_in_db)
-					product = Product(product_name=name, product_quantity=quantity, product_price=price, date_updated=date)
-					session.add(product)
+					product_in_db.product_quantity = quantity
+					product_in_db.product_price = price
+					product_in_db.date_updated = date
 					session.commit()
+
+def app():
+	while True:
+		choice = menu()
+		choice = choice.lower()
+		if choice == 'v':
+			id_options = []
+			for product in session.query(Product):
+				id_options.append(product.product_id)
+			id_error = True
+			while id_error:
+				id_choice = input(f"""
+					\rID options: {id_options}
+					\rEnter the product ID of the item you would like to view:  """)
+				id_choice = clean_id(id_choice, id_options)
+				if type(id_choice) == int:
+					id_error = False 
+			display_product(id_choice)
+			input("\nPress enter to return to menu.")
+		elif choice == 'a':
+			# add product
+			add_product()
+		elif choice == 'b':
+			# create csv file backup
+			backup()
+			input("\nBackup has been created. Press enter to return to menu.")
+		elif choice == 'q':
+			print("\nGoodbye!\n")
+			return None
+
 
 if __name__ == "__main__":
 	Base.metadata.create_all(engine)
-	# add_csv():
+	add_csv()
 	app()
-
-
-
-
-
